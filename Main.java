433aa15b72128d02372a37e4b7ff1a4b59b8cb87
DannyM2027 <daniel.matut2027@gbwl.org>
2025-11-14 13:54:10 +0000

    intro toWHile loops

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+     
+
+      //WHILE  LOOPS REPEAT INSTRUCTIONS in the loop body as long condition is true
+
+      // counter controlled while loop
+      // step 1 initialize conrole varible
+
+
+      int count = 6;
+      // step 2, test condition
+      while (count <= 7) {
+         // loop body executes WHILE count <= 5 is true
+         System.out.println(count);
+
+         count++;
+      }
+
+
    }
 }
