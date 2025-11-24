38d292a9a2f960fd37dee894fb1a7070649e470b
DannyM2027 <daniel.matut2027@gbwl.org>
2025-11-24 14:35:14 +0000

    1.10

--- Main.java
+++ Main.java
@@ -36,8 +36,29 @@ for (int i = 5; i > 0; i--) {
             System.out.println("BLAST OFFFF");
           }
       }
-   
 
+      //lops and strings
+      // use loop to iterate characters 
+      // in string object (traverse)
+
+      // us foor lop to visit every character
+      String name = "daniel";
+      int finalIndex = name.length();
+      System.out.println("Findal Index: " + finalIndex);
+
+      // build a reversed String using loop
+      String reversed = "";
+      for (int i = finalIndex - 1; i >= 0; i--) {
+         System.out.println("Current Index: " + i);
+         String currentLetter = name.substring(i, i+1);
+         System.out.println("Current letter: " + currentLetter);
+         reversed += currentLetter;
+      }
+      System.out.println("Your name backwards: " + reversed);
+
+
+      // while loops can handle tasks other than when yk how many times to iterate
+      // find and replace char until all replaced
 
 
 
